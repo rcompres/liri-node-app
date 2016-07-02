023c7893e4cb1f6ba17ebd013341158ff7d2b24a
ruben compres <ruben compres>
2016-07-02 17:14:14 -0500

    the liri app with twitter only... seems to be syntax error i cannot find

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log("this is loaded");
+
+exports.twitterKeys={
+	consumer_key: 'hcZY8g38LQU73nyL00ELVApVY',
+	consumer_secret: '	ZOMTOfyTTwjdT0zCE0z1Y74G0ebeLZmD5Rt9W5GjNAKjkzHcAV',
+	access_token_key: '1220364840-9mjGwx9gkLbwa7MowKv1DOmRUYpjpNVFvk5pUdb',
+	access_token_secret: '	fRLMtQnj9S0PtK4U6jJ5oKyZc2X0sLplmvLhjAM0s5rNr',
+}
---
+++ liri.js
@@ -0,0 +1,39 @@
+//the variable to pull the key from
+var twitterKey = require(keys.js);
+
+var Twitter = require('twitter');
+
+var client = new Twitter({
+	consumer_key: keyData.twitterKeys.consumer_key,
+	consumer_secret: keyData.twtterKeys.consumer_secret,
+	access_token_key: keyData.twitterKeys.access_token_key,
+	access_token_secret:keyData.twitterKeys.access_token_secret
+});
+//Detecting the input
+var userCommandOne = process.argv[2];
+var userCommandTwo = process.argv[3];
+
+function twitterTweets() {
+if (userCOmmandOne == "my-tweets") {
+	var params = {
+		screen_name:'r_compres',
+		count: 20,
+		trim_user: true
+
+	};
+
+	client.get('statuses/user_timeline',params,function(error, tweets, response){
+		if (!error) {
+			for (var i=0; i<20; i++) {
+				console.log("Tweet posted at: "+ tweets[i].created_at + "\n"+ "Tweet Number: " + (i + 1) "\n" +tweets[i].text + "\n");
+			}
+		} else{
+			console.log("Error! Cannot Compute! must destroy all Humans ");
+		}
+	});
+} else {
+	console.log("Not a known command, try again!");
+}
+};
+
+twitterTweets();
